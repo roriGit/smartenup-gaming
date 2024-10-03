7674522788138efe569a75f9f285bf74d42b61e7
rorisang <rori.madonko@gmail.com>
2024-10-03 23:01:39 +0200

    Updatee component incl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,23 @@ import { BannerComponent } from "./banner/banner.component";
 import { NavmenuComponent } from "./navmenu/navmenu.component";
 import { FavgamesComponent } from "./favgames/favgames.component";
 import { VirtualgamingComponent } from "./virtualgaming/virtualgaming.component";
+import { TopgamesComponent } from "./topgames/topgames.component";
+import { TestimonialsComponent } from "./testimonials/testimonials.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BannerComponent, NavmenuComponent, FavgamesComponent, VirtualgamingComponent],
+  imports: [
+    RouterOutlet,
+    BannerComponent,
+    NavmenuComponent,
+    FavgamesComponent,
+    VirtualgamingComponent,
+    TopgamesComponent,
+    TestimonialsComponent,
+    FooterComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
